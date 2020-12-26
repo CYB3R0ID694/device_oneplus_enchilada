d89e8e946d4dbc5a30cf6ce6c1a2620c4b46d4d3
chandu078 <chandudyavanapelli03@gmail.com>
2020-12-26 19:08:19 +0530

    enchilada: Build with gapps always
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- cherish_enchilada.mk
+++ cherish_enchilada.mk
@@ -30,6 +30,7 @@ TARGET_BOOT_ANIMATION_RES := 1080
 # Gapps
 TARGET_GAPPS_ARCH := arm64
 TARGET_INCLUDE_WIFI_EXT := true
+CHERISH_WITHGAPPS := true
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := cherish_enchilada
