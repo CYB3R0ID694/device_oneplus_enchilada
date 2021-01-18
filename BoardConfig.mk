b3da5ff40794b5564566263a0e213c8d893885ce
chandu078 <chandudyavanapelli03@gmail.com>
2021-01-18 22:05:59 +0530

    enchilada: bluetooth: Move bdroid header to sdm845-common Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,8 +22,5 @@ DEVICE_PATH := device/oneplus/enchilada
 # Assert
 TARGET_OTA_ASSERT_DEVICE := OnePlus6
 
-# Bluetooth
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth/include
-
 # inherit from the proprietary version
 -include vendor/oneplus/enchilada/BoardConfigVendor.mk
