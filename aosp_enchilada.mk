67faab9b9059d589cbd435d7d929ef9e364cc236
zahidmgsk <zahidmgsk@gmail.com>
2021-02-01 08:17:19 +0000

    enchilada: Bring up Pixel Experience-R
    Signed-off-by: zahidmgsk <zahidmgsk@gmail.com>

--- aosp_enchilada.mk
+++ aosp_enchilada.mk
@@ -22,7 +22,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/oneplus/enchilada/device.mk)
 
 # Inherit some common CherishOS stuff.
-$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Boot Animation
 TARGET_BOOT_ANIMATION_RES := 1080
@@ -30,17 +30,15 @@ TARGET_BOOT_ANIMATION_RES := 1080
 # Gapps
 TARGET_GAPPS_ARCH := arm64
 TARGET_INCLUDE_WIFI_EXT := true
-CHERISH_WITHGAPPS := true
 
-# Official
-CHERISH_BUILD_TYPE:= OFFICIAL
+#Official
 
 # Maintainer
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.cherish.maintainer=Zahidm
+#PRODUCT_PROPERTY_OVERRIDES += \
+#    ro.cherish.maintainer=Zahidm
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := cherish_enchilada
+PRODUCT_NAME := aosp_enchilada
 PRODUCT_DEVICE := enchilada
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
